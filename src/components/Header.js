f75c4b6e35f8f3c477e8819a02f7b68926df2668
Dhruv Singla <singladhruv18@gmail.com>
2022-06-21 15:22:12 +0530

    tailwind config

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <div className=' text-neutral-50 text-3xl'>
+          REd COdE
+      </div>
+    )
+  }
+}
+
+export default Header
