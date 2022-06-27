9e7c590a5ff498785150543334ba002f8eae50a0
Shubhveer Singh Chaudhary <shubhchaudhary1203@gmail.com>
2022-06-27 20:01:00 +0530

    minor changes

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -14,6 +14,7 @@ export default function Login() {
     const [ErrorMsg, setErrorMsg] = useState();
     const navigate=useNavigate()
     const [user, loading, error] = useAuthState(auth);
+    const [Bg,setBg]=useState('bg-slate-400')
     useEffect(() => {
         if (loading) {
             
@@ -23,12 +24,16 @@ export default function Login() {
         
     }, [user,loading]);
 
+    const goBack = () => {
+        navigate('/')
+    }
+    const check=()=>setBg("bg-green-200")
 
     return (
         <div className="h-screen flex justify-center items-center">
 
-            <div className='bg-slate-400  w-full mx-10 lg:w-1/2 flex flex-col lg:flex-row rounded-lg opacity-75 py-5 relative'>
-                <Dots />
+            <div className={` ${Bg} w-full mx-10 lg:w-1/2 flex flex-col lg:flex-row rounded-lg opacity-75 py-5 relative duration-300`}>
+                <Dots cross={goBack} check={check} minus={()=>setBg('bg-slate-400')} />
                 <div className='w-full lg:w-2/3 relative'>
 
                     <span className='text-5xl ml-6 text-gray-700'>Sign In</span>
@@ -39,7 +44,7 @@ export default function Login() {
                                 value={Email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 placeholder='Email'
-                                className=' mt-3 rounded bg-slate-400 shadow-lg drop-shadow outline-1 placeholder-gray-700 appearance-none focus:outline-none focus:placeholder-slate-400   focus:bg-gray-600 focus:text-neutral-50 p-1 w-full lg:w-2/3'
+                                className={`mt-3 rounded ${Bg} shadow-lg drop-shadow outline-1 placeholder-gray-700 appearance-none focus:outline-none focus:placeholder-slate-400   focus:bg-gray-600 focus:text-neutral-50 p-1 w-full lg:w-2/3`}
                             />
                         </label>
                     </form>
@@ -50,12 +55,12 @@ export default function Login() {
                                 value={Password}
                                 onChange={(e) => setPassword(e.target.value)}
                                 placeholder='Password'
-                                className=' mt-3 rounded bg-slate-400 shadow-lg drop-shadow outline-1 placeholder-gray-700 appearance-none focus:outline-none focus:placeholder-slate-400 focus:bg-gray-600 focus:text-neutral-50 p-1 w-full lg:w-2/3'
+                                className={` mt-3 rounded ${Bg} shadow-lg drop-shadow outline-1 placeholder-gray-700 appearance-none focus:outline-none focus:placeholder-slate-400 focus:bg-gray-600 focus:text-neutral-50 p-1 w-full lg:w-2/3`}
                             />
                         </label>
                         <span className='text-red-600 ml-7'>{ErrorMsg}</span>
                     </form>
-                    <button onClick={() => logInWithEmailAndPassword(Email, Password)} className='ml-7 w-1/3 text-xl py-1 rounded bg-slate-500 shadow-lg drop-shadow  delay-150 hover:-translate-y-[5px] duration-200 ease-linear'>
+                    <button onClick={() => logInWithEmailAndPassword(Email, Password)} className={`ml-7 w-1/3 text-xl py-1 rounded ${Bg==='bg-slate-400'?"bg-slate-500":"bg-green-400"} shadow-lg drop-shadow  delay-150 hover:-translate-y-[5px] duration-200 ease-linear`}>
                         Submit
                     </button>
 
@@ -63,7 +68,7 @@ export default function Login() {
                 </div>
                 <div className='h-40'>
                     Sign In with
-                    <button onClick={signInWithGoogle} className='ml-7 w-1/3 text-xl py-1 rounded bg-slate-500 shadow-lg drop-shadow  delay-150 hover:-translate-y-[5px] duration-200 ease-linear'>
+                    <button onClick={signInWithGoogle} className={`ml-7 w-1/3 text-xl py-1 rounded ${Bg==='bg-slate-400'?"bg-slate-500":"bg-green-400"} shadow-lg drop-shadow  delay-150 hover:-translate-y-[5px] duration-200 ease-linear`}>
                         ass
                     </button>
                 </div>
