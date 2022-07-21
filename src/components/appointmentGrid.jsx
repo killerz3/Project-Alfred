b2257f85fa96644f10077493226ee7bcd70ce2cd
Dhruv Singla <singladhruv18@gmail.com>
2022-07-21 13:10:55 +0530

    tooltip test

--- src/components/appointmentGrid.jsx
+++ src/components/appointmentGrid.jsx
@@ -4,9 +4,9 @@ import AppointmentCard from './designs/AppointmentCard'
 export default function AppointmentGrid() {
   return (
     <div>
-      <div className={`grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m-5 sm:m-4 duration-300 ease-in`}>
+      <div className={`grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 m-5 sm:m-4 duration-300 ease-in`}>
                   <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
-                  <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+                  <AppointmentCard title='meeting with your mom' time='9:11 AM'/>
                   <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
                   <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
                   <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,32 +1,44 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import {  auth,logout } from '../backend/firbase';
 import { useAuthState } from "react-firebase-hooks/auth";
+import Dots from "./designs/dots";
+import NotFound from "../pages/404";
 export default function Sidebar() {
     const [Open, setOpen] = useState(false);
     const [Item, setItem] = useState(false);
 
     const [user, loading, error] = useAuthState(auth);
     const navigate=useNavigate()
+    const location = useLocation()
 
     useEffect(() => {
         
         if (!user) {
             navigate('/',{replace:true})
         }
+        if(loading){return NotFound}
         
-    }, [user]);
+    }, [user,loading]);
+
+
 
     return (
         <div
-            className={`${Open ? "w-96 md:w-72" : "w-20 sm:w-28 md:w-20"
-                } bg-slate-900  text-neutral-100  relative duration-300 ease-in min-h-screen inline-block`}
+        className={`${Open ? "w-96 md:w-72" : "w-20 sm:w-28 md:w-20"
+    } bg-slate-900  text-neutral-100  relative duration-300 ease-in min-h-screen inline-block`}
+
+
         >
             <button
                 onClick={() => {
                     setOpen(!Open)
-                    setTimeout(()=>{setItem(!Item)},300)
+                    if (!Open){
+                    setTimeout(()=>{setItem(!Item)},300)}
+                    else{
+                        setItem(!Item)
+                    }
                 }}
                 className="bg-slate-400 rounded-full h-10 w-10 absolute top-5 -right-3 translate-x-16}"
             >
@@ -36,14 +48,15 @@ export default function Sidebar() {
                     <i class="fa fa-chevron-left" aria-hidden="true"></i>
                 )}
             </button>
-            <div className="absolute top-20 flex justify-center items-center flex-col gap-2 w-full">
-                <Link to='/dashboard/new-meet' className="border border-slate-100 rounded-lg px-3 py-1 flex justify-center  items-center gap-1 "><i class="fa-solid fa-calendar-plus h-4 w-4"></i><span className={`${Item?"":"hidden"} duration-300 delay-150`}>New Meeting</span></Link>
-                <Link to='/dashboard/settings' className="border border-slate-100 rounded-lg px-3 py-1 flex justify-center  items-center gap-1 "><i className="fa-solid fa-calendar"></i><span className={`${Item?"":"hidden"} duration-300 delay-150`}>Bookings</span></Link>
-                <Link to='/' className="border border-slate-100 rounded-lg px-3 py-1 flex justify-center  items-center gap-1 "><i className="fa-solid fa-calendar"></i><span className={`${Item?"":"hidden"} duration-300 delay-150`}>Bookings</span></Link>
-                <button onClick={logout} className="border border-slate-100 rounded-lg px-3 py-1 flex justify-center  items-center gap-1 "><i className="fa-solid fa-arrow-right-from-bracket"></i><span className={`${Item?"":"hidden"} duration-300 delay-150`}>logout</span></button>
+            <div className="absolute top-20 flex justify-center p-1 md:p-2 items-center flex-col gap-2 w-full">
+                <Link to='/dashboard' className="border border-slate-100 rounded-lg w-full px-3 py-1 flex justify-center  items-center gap-1 text-sm md:text-md  "><i className="fa-solid fa-calendar-days"></i><span className={`${Item?"":"hidden"} duration-300 delay-150`}>Active Meetings</span></Link>
+                <Link to='/dashboard/settings' className="border border-slate-100  w-full rounded-lg px-3 py-1 flex justify-center  items-center gap-1 "><i className="fa-solid fa-calendar-check"></i><span className={`${Item?"":"hidden"} duration-300 delay-150`}>all meetings</span></Link>
+                <Link to='/' className="border border-slate-100 rounded-lg px-3 py-1  w-full flex justify-center  items-center gap-1 "><i className="fa-solid fa-gear"></i><span className={`${Item?"":"hidden"} duration-300 delay-150`}>settings</span></Link>
+                <button onClick={logout} className="border border-slate-100 rounded-lg w-full px-3 py-1 flex justify-center  items-center gap-1 "><i className="fa-solid fa-arrow-right-from-bracket"></i><span className={`${Item?"":"hidden"} duration-300 delay-150`}>logout</span></button>
                 
             </div>
 
         </div>
     );
 }
+
